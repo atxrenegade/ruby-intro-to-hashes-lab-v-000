0b8a54cb6319c3941d8bfb8eedc13b87590ce3d4
Harleigh Abel <harleighabel@gmail.com>
2018-02-09 01:44:28 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,6 +50,7 @@ def monopoly_with_fourth_tier
       names: {
         reading_railroad: {
           "mortgage_value" => "$100" 
+          :pennsylvania_railroad.to_s => "$200"
         },
         pennsylvania_railroad: {},
         b_and_o_railroad: {},
