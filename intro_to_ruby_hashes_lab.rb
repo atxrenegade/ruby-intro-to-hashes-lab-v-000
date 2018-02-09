b4d2324fe881155c975ca3c04f95be0acb266eb9
Harleigh Abel <harleighabel@gmail.com>
2018-02-09 01:27:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,12 @@ def monopoly_with_second_tier
 end 
 
 def monopoly_with_third_tier
-  monopoly[:railroads][:rent_in_dollars] => one_piece_owned: 25
+  monopoly = {
+    railroads: {
+      rent_in_dollars: {
+        one_piece_owned: 25
+      }
+    }
+  }
   
 end   
